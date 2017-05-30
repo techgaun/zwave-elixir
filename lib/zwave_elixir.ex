2ea15d20772e5632d70fd47e02c0608ebf898e4d
techgaun <coolsamar207@gmail.com>
2017-05-30 14:31:34 -0500

    cleanup

--- lib/zwave_elixir.ex
+++ lib/zwave_elixir.ex
@@ -1,18 +1,9 @@
 defmodule ZwaveElixir do
   @moduledoc """
-  Documentation for ZwaveElixir.
+  ZwaveElixir is a pure Elixir library for working with ZWave Network and devices.
+
+  ZwaveElixir allows you to interface with the serial device (zwave controller) and
+  talk with the zwave network through ZwaveElixir modules.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ZwaveElixir.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
